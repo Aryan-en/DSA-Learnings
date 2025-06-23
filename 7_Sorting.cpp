50a454861872c6f9debed97550542dc39a529ced
Aryan Singh <aryan.singh.en.1142@gmail.com>
2025-06-23 17:07:08 +0530

    Updated more

--- 7_Sorting.cpp
+++ 7_Sorting.cpp
@@ -11,9 +11,53 @@
 #include <functional>
 #include <stack>
 using namespace std;
-void SelectionSort(){
     int arr[6] = {13,46,24,52,20,9};
+    int mini, temp;
+
+void SelectionSort(){
+// Sorting does by swapping minimums at first indexes
+   
+    for (int i = 0; i < 6; i++)
+    {   
+        mini = i;
+        for (int j = i; j < 6; j++)
+        {
+            if(arr[j] < arr[mini]){
+                mini = j;
+            }
+            temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
+        }
+        
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        cout << arr[i] << ", ";
+    }
+}
+void BubbleSort(){
+// Adjacent elements are swapped such that the maximum 
+// pushes to the last
+    for (int i = 5; i >=0; i--)
+    {
+        for (int j = 0; j <= (i-1); j++)
+        {
+            if (arr[j] > arr[j+1]){
+                temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        
+    }
+    
+    for (int i = 0; i < 6; i++)
+    {
+        cout << arr[i] << ", ";
+    }
 }
 int main() {
+BubbleSort();
 return 0;
 }
