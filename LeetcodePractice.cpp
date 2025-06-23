a1975b9ba882b0c39a5437f744a05ffc88f2be3a
Aryan Singh <aryan.singh.en.1142@gmail.com>
2025-06-23 18:21:04 +0530

    Updated more

--- LeetcodePractice.cpp
+++ LeetcodePractice.cpp
@@ -11,32 +11,50 @@
 #include <functional>
 #include <stack>
 using namespace std;
-string newstr;
-int generateTag(string caption) {
-        newstr += '#';
-        for (int i = 0; i < caption.length(); i++)
-        {
-            if(caption[i] == ' '){
-                caption[i+1] = toupper(caption[i+1]);
-                newstr += caption[i+1];
-                caption.erase(i+1,1);
-            }
-            else{
-                caption[i] = tolower(caption[i]);
-                newstr += caption[i];  
-            }
-        }
-     for (int i = 0; i < newstr.length(); i++)
-     {
-        if(newstr[i] == '\\'){
-            caption.erase(caption[i], 6);
-        }
-     }
+// string newstr;
+// int generateTag(string caption) {
+//         newstr += '#';
+//         for (int i = 0; i < caption.length(); i++)
+//         {
+//             if(caption[i] == ' '){
+//                 caption[i+1] = toupper(caption[i+1]);
+//                 newstr += caption[i+1];
+//                 caption.erase(i+1,1);
+//             }
+//             else{
+//                 caption[i] = tolower(caption[i]);
+//                 newstr += caption[i];  
+//             }
+//         }
+//      for (int i = 0; i < newstr.length(); i++)
+//      {
+//         if(newstr[i] == '\\'){
+//             caption.erase(caption[i], 6);
+//         }
+//      }
         
-    cout << '\\'; 
-    return 0;
+//     cout << '\\'; 
+//     return 0;
+// }
+
+vector<int> nums = {1,3,5,6};
+int target = 7;
+void searchInsert() {
+    int ExistingIndex = 0;
+    vector<int> newarr;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if(target == nums[i]){
+            ExistingIndex += i;
+            // return ExistingIndex;
+        }
+        else{
+            newarr.emplace_back(nums[i]);
+        }
+    }
 }
+
 int main() {
-generateTag("Bold apple beyond bright future crash mountains glow light gently dance waits shore breeze mind ");
+searchInsert();
 return 0;
 }
