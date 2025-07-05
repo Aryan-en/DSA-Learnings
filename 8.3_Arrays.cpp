fc859c2d64b0ab3dedcf89ee00d8a77527bc318b
Aryan Singh <aryan.singh.en.1142@gmail.com>
2025-07-06 03:21:05 +0530

    Added more

--- 8.3_Arrays.cpp
+++ 8.3_Arrays.cpp
@@ -11,8 +11,183 @@
 #include <functional>
 #include <stack>
 using namespace std;
+// void subarraySum(vector<int>& nums, int k) {
+//     int count = 0;
+//    for (int i = 0; i < nums.size(); i++)
+//    {    
+//         int s = 0;
+//         for (int j = i; j < nums.size(); j++)
+//         {
+//             s += nums[j];
+//             if(s == k){
+//                 count++;
+//             }
+//         }
 
-int main() {
+//    }
+//     cout << count;
+// }
+// void maxSubArray(vector<int>& nums) {  
+// Kadane's Algo just keep moving forward and if sum<0 just drop it 
+//     int sum = 0, maxm = INT_MIN;
+//     for (int i = 0; i < nums.size(); i++)
+//     {
+//         sum += nums[i];
+//         if(sum > maxm){
+//             maxm = sum;
+//         }
+//         if(sum<0){
+//             sum = 0;
+//         }
+//     }
+//     cout << maxm;
     
+// }
+// bool check(vector<int>& nums){
+//     vector <int> sortedArr;
+//     bool res = false;
+//     int n = nums.size();
+//     for(auto itr : nums){
+//         sortedArr.emplace_back(itr);
+//     }
+//     sort(sortedArr.begin(), sortedArr.end());
+//    for (int k = 0; k < nums.size()-1; k++)
+//    {
+//         k%= n;
+//         reverse(sortedArr.begin(), sortedArr.end());
+//         reverse(sortedArr.begin(), sortedArr.begin()+k);
+//         reverse(sortedArr.begin()+k, sortedArr.end());
+//         if (nums == sortedArr)
+//         {
+//             res = true;
+//         }   
+//    }
+//    return res;
+// }
+// void nextPermutation(vector<int>& nums) {
+//       int ind1=-1;
+//         int ind2=-1;
+//         // step 1 find breaking point 
+//         for(int i=nums.size()-2;i>=0;i--){
+//             if(nums[i]<nums[i+1]){
+//                 ind1=i;
+//                 break;
+//             }
+//         }
+//         // if there is no breaking  point 
+//         if(ind1==-1){
+//             reverse(nums,0);
+//         }
+        
+//         else{
+//             // step 2 find next greater element and swap with ind2
+//             for(int i=nums.size()-1;i>=0;i--){
+//                 if(nums[i]>nums[ind1]){
+//                     ind2=i;
+//                     break;
+//                 }
+//             }
+
+//             swap(nums,ind1,ind2);
+//             // step 3 reverse the rest right half
+//             reverse(nums,ind1+1);
+//         }
+//     }
+//     void swap(vector <int> &nums,int i,int j){
+//         int temp=nums[i];
+//         nums[i]=nums[j];
+//         nums[j]=temp;
+//     }
+//     void reverse(vector <int> &nums,int start){
+//         int i=start;
+//         int j=nums.size()-1;
+//         while(i<j){
+//             swap(nums,i,j);
+//             i++;
+//             j--;
+//         }
+// }
+// void setZeroes(vector <vector<int>> &nums){
+    
+// }
+// int firstMissingPositive(vector<int>& nums) {
+//         int n = 1;
+//         sort(nums.begin(), nums.end());
+//         int largest = nums[nums.size()-1];
+//         for (int i = 1; i <= largest+1; i++)
+//         {
+//             cout << i << endl;
+            
+//         }
+//        return n; 
+// }
+// void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//     vector <int> mergedarr;
+//     for (int i = 0; i < m; i++)
+//     {
+//         mergedarr.emplace_back(nums1[i]);
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         mergedarr.emplace_back(nums2[i]);
+//     }
+//     sort(mergedarr.begin(), mergedarr.end());
+//     for(auto itr : mergedarr){
+//         cout << itr << " ";
+//     }
+// }
+// void spiralOrder(vector<vector<int>>& matrix) {
+//     vector <int> ans;
+//     int columns = matrix[0].size();
+//     int rows = matrix.size();
+//     for (int i = 0; i < columns; i++)
+//     {
+//         ans.emplace_back(matrix[0][i]);
+//     }
+//     for (int i = 1; i < rows; i++)
+//     {
+//         ans.emplace_back(matrix[i][columns-1]);
+//     }
+//     for (int i = columns-2; i >= 0; i--)
+//     {
+//         ans.emplace_back(matrix[rows-1][i]);
+//     }
+//     for (int i = 0; i < count; i++)
+//     {
+//         /* code */
+//     }
+    
+//     for(auto itr: ans){
+//         cout << itr << " ";
+//     }
+    
+// }
+void firstMissingPositive(vector<int>& nums) {
+        int n = 1;
+        vector <int> fmp;
+        sort(nums.begin(), nums.end());
+        int largest = nums[nums.size()-1];
+        for (int i = 1; i <= largest; i++)
+        {
+            fmp.emplace_back(i);
+        }
+        for (auto itr1 : fmp)
+        {
+            for (auto itr2: nums)
+            {
+                
+                if(itr1 != itr2){
+                    cout << itr1 << " " << itr2 << endl;
+                    n = itr1;
+                    break;
+                }
+            }
+            
+        }
+        cout << n;
+}
+int main() {
+vector <int> matrix = {7,8,9,11,12};
+firstMissingPositive(matrix);
 return 0;
 }
---
+++ 8.4_Arrays.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cmath>
+#include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <list>
+#include <map>
+#include <string>
+#include <functional>
+#include <stack>
+#include <set>
+using namespace std;
+// void rotate(vector<vector<int>>& matrix) {
+//         int rows = matrix.size();
+//         int columns = matrix[0].size();
+//         vector<vector<int>> rotated;
+
+// }
+// int thirdMax(vector<int>& nums) {
+//         set<int> st;
+//         vector<int> arr;
+//         int n;
+//         for(auto itr : nums){
+//             st.insert(itr);
+//         }
+//         for(auto str : st){
+//             arr.emplace_back(str);
+//         }
+//         if(st.size()== 2){
+//             n = arr[1];
+//         }
+//         else{
+//             n = arr[2];
+//         }
+//         return n;
+// }
+// void intersect(vector<int>& nums1, vector<int>& nums2) {
+// //                     sort(nums1.begin(), nums1.end());
+//         sort(nums2.begin(), nums2.end());
+        
+//         int i = 0, j = 0;
+//         vector<int> result;
+        
+//         while (i < nums1.size() && j < nums2.size()) {
+//             if (nums1[i] < nums2[j]) {
+//                 i++;
+//             } else if (nums1[i] > nums2[j]) {
+//                 j++;
+//             } else {
+//                 result.push_back(nums1[i]);
+//                 i++;
+//                 j++;
+//             }
+//         }
+        
+//         return result;
+
+// }
+// void findLucky(vector<int>& arr) {
+//     map <int, int> hash;
+//     for (int i = 0; i < arr.size(); i++)
+//     {
+//         hash[arr[i]]++;
+//     }
+//     int n;
+//     for(auto itr : hash){
+//         if(itr.first == itr.second){
+//             n = itr.first;
+//         }
+//     }
+//     cout << n;
+// }
+void topKFrequent(vector<int>& nums, int k) {
+    map <int, int> hash;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        hash[nums[i]]++;
+    }
+    int n = 0;
+    vector <int> res;
+    for(auto itr : hash){
+        
+    }
+}
+int main() {
+// vector <vector<int>> matrix = {{1,2,3},
+//                                {4,5,6},
+//                                {7,8,9}};
+vector<int> arr = {2,2,3,4};
+topKFrequent(arr, 2);
+return 0;
+}
