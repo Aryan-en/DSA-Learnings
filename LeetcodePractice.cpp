839fe449646e5cc29c71aa6b3d6fca75bbbc9908
Aryan Singh <aryan.singh.en.1142@gmail.com>
2025-12-12 17:06:02 +0530

    Updated More

--- LeetcodePractice.cpp
+++ LeetcodePractice.cpp
@@ -12,11 +12,26 @@
 #include <stack>
 #include <set>
 using namespace std;
-    void findShortestSubArray(vector<int>& nums) {
-        
+void uniqueOccurrences(vector<int>& arr) {
+    map <int, int> countMap;
+    map <int, int> countMap2;
+    bool ans = true;
+    for (int num : arr) {
+        countMap[num]++;
     }
+    for(auto itr : countMap){
+        countMap2[itr.second]++;
+    }
+    for(auto itr : countMap2){
+        if(itr.second > 1){
+            ans = false;
+            break;
+        }
+    }
+    // return ans;
+}
 int main() {
-vector<int> nums = {1, 2, 2, 3, 1};
-findShortestSubArray(nums);
+    vector<int> arr = {1, 2, 2, 1, 1, 3};
+    uniqueOccurrences(arr);
 return 0;
 }
