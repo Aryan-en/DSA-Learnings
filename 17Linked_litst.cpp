c15da982d93a4e4231c9b0a7a85b6bc34505fc46
Aryan Singh <aryan.singh.en.1142@gmail.com>
2026-01-03 14:45:45 +0530

    Started with Linked List

--- 17Linked_litst.cpp
+++ 17Linked_litst.cpp
@@ -12,8 +12,22 @@
 #include <stack>
 #include <set>
 using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data1, Node* next1){
+        data1 = data;
+        next1 = next;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
 
 int main() {
-    
+    Node* head=new Node(1, nullptr);
+    cout << head->data << endl;
 return 0;
 }
