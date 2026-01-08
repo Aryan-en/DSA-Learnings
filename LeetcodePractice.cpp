927e061a4b57c6b61f2b9c9f601c4c46e4c38af3
Aryan Singh <aryan.singh.en.1142@gmail.com>
2026-01-08 15:54:10 +0530

    Leetcode

--- LeetcodePractice.cpp
+++ LeetcodePractice.cpp
@@ -12,17 +12,11 @@
 #include <stack>
 #include <set>
 using namespace std;
-int hIndex(vector<int>& citations) {
-    sort(citations.begin(), citations.end());
-    int n = citations.size();
-    int h = n;
+void strStr(string haystack, string needle) {
     
-    cout << endl;
-    return h;
 }
-int main()
-{
-    vector<int> citations = {3, 0, 6, 1, 5};
-    cout << hIndex(citations) << endl;
-    return 0;
+int main() {
+string haystack = "sadbutsad", needle = "sad";
+strStr(haystack, needle);
+return 0;
 }
